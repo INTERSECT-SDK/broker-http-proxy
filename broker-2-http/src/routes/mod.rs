719a89208d75e0e6e32380d2b056294434790815
Lance-Drane <ldraneutk@gmail.com>
2025-02-13 22:38:21 -0500

    #1 - add publish endpoint for broker-2-http, start using amqp connection pool
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

--- broker-2-http/src/routes/mod.rs
+++ broker-2-http/src/routes/mod.rs
@@ -1,3 +1,4 @@
 pub mod health_check;
 pub mod not_found;
+pub mod publish;
 pub mod subscribe;
--- http-2-broker/src/configuration.rs
+++ http-2-broker/src/configuration.rs
@@ -27,4 +27,6 @@ pub struct Settings {
     pub log_level: LogLevel,
     /// set to true for developer-unfriendly settings (currently just log formats)
     pub production: bool,
+    /// this should only contain the SYSTEM prefix, i.e. "organization.facility.system"
+    pub topic_prefix: String,
 }
